e58b853bab74aec93c005b5e21e2168ca28bf0ad
Kamil Chlebek <kamil.chlebek94@gmail.com>
2018-01-19 23:47:17 +0100

    style ++

--- src/main/java/pl/edu/agh/student/StyleApp.java
+++ src/main/java/pl/edu/agh/student/StyleApp.java
@@ -1,19 +1,26 @@
 package pl.edu.agh.student;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import org.deeplearning4j.models.paragraphvectors.ParagraphVectors;
 import pl.edu.agh.student.model.Tweet;
 import pl.edu.agh.student.style.*;
 
 import java.io.FileNotFoundException;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StyleApp {
 
     public static void main(String[] args) throws JsonProcessingException, FileNotFoundException {
 
         TweetReader tweetReader = new TweetReader();
-        List<Tweet> read = tweetReader.read("tweets_oneline_1000.csv");
+
+        List<Tweet> read = Stream.of("JaroslawKuzniar", "BoniekZibi", "robertbiedron", "sikorskiradek")
+                .map(user -> tweetReader.read("tweets_" + user + "_train.csv"))
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
 
         StyleDataPreparer styleDataPreparer = new StyleDataPreparer(read);
         List<Tweet> tweets = styleDataPreparer.prepare();
@@ -21,8 +28,28 @@ public class StyleApp {
         List<String> texts = tweets.stream().map(Tweet::getText).collect(Collectors.toList());
         List<String> users = tweets.stream().map(Tweet::getUser).collect(Collectors.toList());
 
-        new StyleAnalyzer(new PartOfSpeechModificator()).run(users, texts, "tree_pos.js");
-        new StyleAnalyzer(new DefaultModificator()).run(users, texts, "tree_default.js");
+        StyleAnalyzer styleAnalyzerPos = new StyleAnalyzer(new PartOfSpeechModificator());
+        ParagraphVectors vcPos = styleAnalyzerPos.run(users, texts, "tree_pos.js");
+
+        StyleAnalyzer styleAnalyzerDefault = new StyleAnalyzer(new DefaultModificator());
+        ParagraphVectors vcDefault = styleAnalyzerDefault.run(users, texts, "tree_default.js");
+
+        // ============================================
+
+        List<Tweet> readTest = Stream.of("JaroslawKuzniar", "BoniekZibi", "robertbiedron", "sikorskiradek")
+                .map(user -> tweetReader.read("tweets_" + user + "_test.csv"))
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+
+        StyleDataPreparer styleDataPreparerTest = new StyleDataPreparer(readTest);
+        List<Tweet> tweetsTest = styleDataPreparerTest.prepare();
+
+
+        System.out.println("POS results");
+        styleAnalyzerPos.test(tweetsTest, vcPos);
+
+        System.out.println("Default results");
+        styleAnalyzerDefault.test(tweetsTest, vcDefault);
 
     }
 
--- src/main/java/pl/edu/agh/student/style/StyleAnalyzer.java
+++ src/main/java/pl/edu/agh/student/style/StyleAnalyzer.java
@@ -3,6 +3,7 @@ package pl.edu.agh.student.style;
 import com.apporiented.algorithm.clustering.Cluster;
 import org.deeplearning4j.models.paragraphvectors.ParagraphVectors;
 import pl.edu.agh.student.model.Hierarchy;
+import pl.edu.agh.student.model.Tweet;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,7 +16,7 @@ public class StyleAnalyzer {
         this.modificator = modificator;
     }
 
-    public void run(List<String> labels, List<String> texts, String filename){
+    public ParagraphVectors run(List<String> labels, List<String> texts, String filename){
 
         texts = texts.stream()
                 .map(this.modificator::modify)
@@ -33,5 +34,32 @@ public class StyleAnalyzer {
         HierarchyJavascriptSaver hierarchyJavascriptSaver = new HierarchyJavascriptSaver(hierarchy);
         hierarchyJavascriptSaver.save("visualise/" + filename);
 
+        return vec;
+
+    }
+
+    public void test(List<Tweet> tweets, ParagraphVectors vc){
+
+        int success = 0;
+        int fail = 0;
+        for (Tweet tweet : tweets) {
+            List<String> strings = (List<String>) vc.nearestLabels(this.modificator.modify(tweet.getText()), 1);
+//            System.out.println(tweet.getUser() + " -> " + strings);
+            if(matchSuccess(tweet, strings)){
+                success++;
+            }
+            else {
+                fail++;
+            }
+        }
+
+        System.out.println("Total: " + tweets.size());
+        System.out.println("Failed: " + fail);
+        System.out.println("Success: " + success);
+        System.out.println("SuccessRatio: " + (double) success/tweets.size());
+    }
+
+    private boolean matchSuccess(Tweet tweet, List<String> strings) {
+        return strings != null && strings.size() > 0 && strings.get(0) != null && tweet.getUser().equals(strings.get(0));
     }
 }
